728a45242bf0f90943b6db04fa97dc2426d689f0
uraghuvamshireddy <uraghuvamshireddy@gmail.com>
2024-08-27 17:05:36 +0530

    qrcode

--- src/vendorDashboard/componants/Qrcode.jsx
+++ src/vendorDashboard/componants/Qrcode.jsx
@@ -37,7 +37,7 @@ const firmId=localStorage.getItem('firmId');
         <div id="qr-code">
                 {/* <QRCode value="localhost:5173/firmId/menu" /> */}
 
-          <QRCode value="https://enchanting-manatee-dd5c07.netlify.app/firmId/menu" />
+          <QRCode value={`https://enchanting-manatee-dd5c07.netlify.app/${firmId}/menu`} />
           {/* <button onClick={downloadQRCode}>Download QR Code</button> */}
         </div>
       )}
