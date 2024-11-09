4f8ec4464d841a2645476286c9573ea73e0152bd
uraghuvamshireddy <uraghuvamshireddy@gmail.com>
2024-11-09 16:36:13 +0530

    qrcode

--- src/vendorDashboard/componants/Qrcode.jsx
+++ src/vendorDashboard/componants/Qrcode.jsx
@@ -21,9 +21,9 @@ const firmId=localStorage.getItem('firmId');
  <div className='gen'> <button onClick={generateQRCode}>Generate QR Code</button></div> 
       {qrIsVisible && (
         <div id="qr-code">
-                <QRCode value="localhost:5173/firmId/menu" />
+                {/* <QRCode value="localhost:5173/firmId/menu" /> */}
 
-          {/* <QRCode  id="qrCodeEl" value={`https://enchanting-manatee-dd5c07.netlify.app/${firmId}/menu`} /> */}
+          <QRCode  id="qrCodeEl" value={`https://enchanting-manatee-dd5c07.netlify.app/${firmId}/menu`} />
           {/* <button onClick={downloadQRCode()}>Download QR Code</button> */}
         </div>
       )}
